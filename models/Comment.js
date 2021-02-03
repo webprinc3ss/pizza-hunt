dcff1aef91df4aadacdd68754a5f8946d673463b
Mystic Kitty <djabranton@gmail.com>
2021-02-03 09:41:10 -0800

    Added validators.

--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,13 @@ const ReplySchema = new Schema(
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: 'You need to write a reply!',
+            trim: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: 'Who are you?',
         },
         createdAt: {
             type: Date,
@@ -30,10 +33,12 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
     {
         writtenBy: {
-            type: String
+            type: String,
+            required: 'Who are you?'
         },
         commentBody: {
-            type: String
+            type: String,
+            required: 'Write something!'
         },
         createdAt: {
             type: Date,
